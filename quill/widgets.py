ac6642a8e4041f5a5d97cc2ee489e021367203ef
Yulya <yulya.portnova@gmail.com>
2017-11-06 16:21:56 +0200

    django 1.11 compatibility

--- quill/widgets.py
+++ quill/widgets.py
@@ -36,7 +36,9 @@ class QuillEditorWidget(forms.Textarea):
         """Render the Quill WYSIWYG."""
         if value is None:
             value = ''
-        final_attrs = self.build_attrs(attrs, name=name)
+        attrs = attrs.copy()
+        attrs['name'] = name
+        final_attrs = self.build_attrs(attrs)
         quill_app = apps.get_app_config('quill')
         quill_config = getattr(quill_app, self.config)
 
